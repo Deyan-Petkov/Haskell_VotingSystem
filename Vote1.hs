5805e623c17f73a2db8eb0dea09c613d6f59e490
Deyan-Petkov <deyan.post@gmail.com>
2020-12-28 00:19:48 +0000

    Need to stop the finding of new winners when there are exactly enough candidates left or we filled up the sits

--- Vote1.hs
+++ Vote1.hs
@@ -14,7 +14,6 @@ readVotes path =  do
     vote <- readFile path 
 
 
-
  ----------First Past The Post System   
     {--In this model of voting we take the first choice from each vote and the candidate with most occurances on first position
     wins the election--}
@@ -68,33 +67,47 @@ quota  = div (length $ firstPreference $ stringToIntVote $ separateVotes poll) s
 
 --[(Int,[Int])] -> [Int]
 
-{--List with the current candidates having enough first preferences--}
-findWinners :: Poll -> Candidates
-findWinners p = takeWhile (>= quota) $ map snd countWithPref
-    where 
-        countWithPref = zip (map length groupedSortedFirstPref ) $ map head  groupedSortedFirstPref -- [(5,1),(2,2),(1,3),(4,4)]
-        groupedSortedFirstPref = group $ sort $ firstPreference p --[[1,1,1,1,1],[2,2],[3],[4,4,4,4]]
+-- {--List with the current candidates having enough first preferences--}
+-- findWinners :: Poll -> Candidates
+-- findWinners p = takeWhile (>= quota) $ map snd countWithPref
+--     where 
+--         countWithPref = zip (map length groupedSortedFirstPref ) $ map head  groupedSortedFirstPref -- [(5,1),(2,2),(1,3),(4,4)]
+--         groupedSortedFirstPref = group $ sort $ firstPreference p --[[1,1,1,1,1],[2,2],[3],[4,4,4,4]]
+
+{--If we have candidate with first preferences above the quota then return it, else remove the one with least first pref --}
+findWinners :: Poll -> Poll
+findWinners p = if topCandidateVotes m >= quota then p else findWinners $ discardLeast m p --  *** ELSE DISCARD AND RUN ELEECTION until we can find topCandidate to return
+    where
+        m = mapCandidates p
+        
 
 {--Check if we have enough winners to take all available seats--}
 enoughWinners :: Candidates -> Bool   
 enoughWinners winners = length winners > seats --  *** to read winners form file instead
 
-
-
-
 {--Delete as many votes, containing winners as first choice, as the value of the quota. We'll need the second choice of the rest of this votes to be assigned to the other votes having this second choice as first.--}
-updateVotes :: Poll -> Int -> Poll  --  ** to write helper funciton that passes candidtaes to updateVotes so we clean for all obtained candidates
-updateVotes p candidate = foldr (:) takeCandidate dropCandidate
-    where -- from all sublists headed by the candidate take only what is 
+updateVotes :: Poll -> Poll  --  ** to write helper funciton that passes candidtaes to updateVotes so we clean for all obtained candidates
+updateVotes p  = cleanEmpty $ map (filter (/= candidate)) bindLists --Discard the winner as does not need to be part of the competition anymore and clean from empty lists
+    where  -- from all sublists headed by the candidate take only what is 
         --above the quota (e.g quota 5 list 12 -> take 7 list headed by by the candidate)
+        bindLists = foldr (:) takeCandidate dropCandidate
         takeCandidate = drop quota $ [ x | x <- p , head x == candidate]
         dropCandidate = [ x | x <- p , head x /= candidate] -- take all sublists without the candidate as first preference
+        candidate = winnerCandidate $ mapCandidates p
+
+{--Discard the winner as does not need to be part of the competition anymore--}
+discardFirst :: Poll -> Poll
+discardFirst p = cleanEmpty j --take all votes witout the empty ones
+    where
+        j = map (filter (/= (winnerCandidate $ mapCandidates p))) p -- clean the votes from the top candidate as he doesn't need to be part of the competition anymore
 -- ---------------------------------------------------------------------
 
 
 {--iterates over the election discarding candidates until one of them obtain majority--}
 drawing :: Poll -> Poll
-drawing p = if winner (mapCandidates p) p then p else drawing (discard (mapCandidates p) p) 
+drawing p = if winner m p then p else drawing (discardLeast m p)
+    where
+        m = mapCandidates p
 
 {--After reading the file containing votes we need to split 
 the votes and candidates from each other in a way that we can
@@ -125,6 +138,13 @@ firstPreference =  map head
 topCandidateVotes :: Map Int Int -> Int 
 topCandidateVotes m  = last $ Set.elems $ Map.keysSet m
 
+leastCandidate :: Map Int Int -> Int
+leastCandidate m = fromMaybe 1 h --extract the candidate with least first preferences from Maybe
+    where
+        h = Map.lookup g m --Just # //the "name" of the candidate that the first/smallest key is pair with
+        g = head $ take 1 $ Map.keys m -- 1 //take the first key from the Map as they are ordered and the least chosen candidate will be at first position in the Map
+       -- m = mapCandidates p
+
 {--returns the winner--}
 winnerCandidate :: Map Int Int -> Int
 winnerCandidate m = fromMaybe 1  $ Map.lookup (last $ Set.elems $ Map.keysSet m) m
@@ -137,13 +157,11 @@ winner w  p = topCandidateVotes w > div (length $ firstPreference p) 2
 
 {--remove the candidate with least first preferences
     in the votes and eventually clean the list from empty votes--}
-discard :: Map Int Int -> Poll -> Poll
-discard m p = cleanEmpty j --take all votes witout the empty ones
+discardLeast :: Map Int Int -> Poll -> Poll
+discardLeast m p = cleanEmpty j --take all votes witout the empty ones
     where
-        j = map (filter (/= i)) p -- clean the votes from the candidate with least first preferences 
-        i = fromMaybe 1 h --extract the candidate with least first preferences from Maybe
-        h = Map.lookup g m --Just # //the "name" of the candidate that the first/smallest key is pair with
-        g = head $ take 1 $ Map.keys m -- 1 //take the first key from the Map as they are ordered and the least chosen candidate will be at first position in the Map
+        j = map (filter (/= leastCandidate m)) p -- clean the votes from the candidate with least first preferences 
+       
 
 {----}
 mapCandidates :: Poll -> Map Int Int
